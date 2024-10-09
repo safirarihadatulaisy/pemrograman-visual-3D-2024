b0577c6a65fa45c9d3d096fed2cf1f70f259ce43
safirarihadatulaisy <rihadatulaisy@gmail.com>
2024-10-09 12:25:41 +0700

    230441100005_SafiraRihadatulAisy_FankyAbdilqoyyim

---
+++ modul-2/230441100005-SafiraRihadatulAisy/Modul2_Cake/src/main/java/com/mycompany/modul2_cake/Modul2_Cake.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modul2_cake;
+
+/**
+ *
+ * @author safira rihadatul a
+ */
+public class Modul2_Cake {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
